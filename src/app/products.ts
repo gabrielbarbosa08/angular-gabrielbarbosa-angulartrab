22bf34c3027e342b8674cc9f7efa2826c8b65685
gabrielbarbosa08 <gabrielbarbosa.ti08@gmail.com>
2020-05-13 17:22:21 -0300

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,19 +2,19 @@ export const products = [
   {
     img: 'https://images-na.ssl-images-amazon.com/images/I/41c2xlXGmYL._AC_.jpg',
     name: 'xiaomi redimi note 8',
-    price: 4999.99,
+    price: 1999.00,
     description: 'Smartphone Xiaomi Redmi Note 8 64GB Versão Global Desbloqueado Preto'
   },
   {
     img:'https://www.casasbahia-imagens.com.br/TelefoneseCelulares/celular/1500948585/1193827345/celular-xiaomi-redmi-7-3gb-32gb-1500948585.jpg',
     name: 'xiaomi redimi note 7',
-    price: 1139.05,
+    price: 1999.00,
     description: 'Celular Xiaomi Redmi 7 3Gb/32Gb'
   },
   {
     img:'https://www.casasbahia-imagens.com.br/TelefoneseCelulares/Smartphones/Android/1501013028/1198348979/redmi-note-8-63-4gb-64gb-dual-sim-moonlight-white-branco-1501013028.jpg',
     name: 'xiaomi redimi note 8 white',
-    price: 4948.20,
+    price: 1999.00,
     description: 'Redmi Note 8 6.3 4gb/64gb Dual Sim – Moonlight White (Branco)'
   }
 ];
